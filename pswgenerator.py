a1adf4f39cec9331275f1c71e09a29792e46a4e3
Deficienzahh <121560901+Deficienzahh@users.noreply.github.com>
2023-02-03 19:41:17 +0100

    Updated the function of the tool
    Now you can choose between a simple password and a complex password

--- pswgenerator.py
+++ pswgenerator.py
@@ -1,12 +1,22 @@
 import random 
 
+
 lower_case = "abcdefghijklmnopqrstuvwxyz"
 upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "0123456789"
+simbols = "*+@$%&£!§-:_"
 
 
-use_for = lower_case + upper_case + numbers 
-lenght_for_pass = 8
+
+print ('Ciao questo tool offre la possibilità di creare password con due diversi livelli di difficolta.')
+
+if input ('Desideri creare una password semplice o complessa? (S/C)') == "C":
+    use_for = lower_case + upper_case + numbers + simbols
+    lenght_for_pass = 20
+else: 
+    use_for = lower_case + numbers
+    lenght_for_pass = 8
+
 
 password = "".join(random.sample(use_for, lenght_for_pass))
 
